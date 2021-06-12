2b70ba4282179d22561ebffcb08404fa4797cdd5
freekmurze <freekmurze@users.noreply.github.com>
2021-06-12 21:55:05 +0000

    Fix styling

--- tests/TestClasses/Jobs/StringTestJob.php
+++ tests/TestClasses/Jobs/StringTestJob.php
@@ -2,15 +2,11 @@
 
 namespace Tests\TestClasses\Jobs;
 
-use Tests\TestClasses\Models\TestModel;
-
 class StringTestJob extends BaseTestJob
 {
     public function __construct(
         public string $myString,
         public string $anotherString
-    )
-    {
-
+    ) {
     }
 }
